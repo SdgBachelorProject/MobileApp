342ed2a368422e09bc1f3b7f71ac7f4f98171c76
WojtekMielczarek-Makimo <wojciech.mielczarek@makimo.pl>
2022-10-09 11:16:52 +0200

    Sign in with google works without proper architecture

--- app/src/main/java/com/example/sdgbachelorproject/MainActivity.kt
+++ app/src/main/java/com/example/sdgbachelorproject/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.example.sdgbachelorproject
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.example.sdgbachelorproject.view.SignInActivity
+import com.firebase.ui.auth.AuthUI
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.btn_log_out)
+
+        button.setOnClickListener {
+            signOut()
+        }
+    }
+
+    private fun signOut() {
+        AuthUI.getInstance()
+            .signOut(this)
+            .addOnCompleteListener {
+                val intent = Intent(this@MainActivity, SignInActivity::class.java)
+                startActivity(intent)
+            }
+    }
+
+    private fun delete() {
+        // [START auth_fui_delete]
+        AuthUI.getInstance()
+            .delete(this)
+            .addOnCompleteListener {
+                // ...
+            }
+        // [END auth_fui_delete]
     }
 }
