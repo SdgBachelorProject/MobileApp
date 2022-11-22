4ab113e0c92135a8ab8ea0910093cdad245e0946
WojtekMielczarek-Makimo <wojciech.mielczarek@makimo.pl>
2022-11-22 17:09:54 +0100

    WIP

--- app/src/main/java/com/example/sdgbachelorproject/view/HomeFragment.kt
+++ app/src/main/java/com/example/sdgbachelorproject/view/HomeFragment.kt
@@ -10,6 +10,7 @@ import com.example.sdgbachelorproject.databinding.FragmentHomeBinding
 import com.example.sdgbachelorproject.viewModel.SignInViewModel
 import com.firebase.ui.auth.AuthUI
 import com.google.firebase.quickstart.auth.kotlin.SignInActivity
+import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.android.synthetic.main.fragment_home.view.*
 import javax.inject.Inject
 
@@ -34,24 +35,35 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val view = binding.root
 
-        view.btn_log_out.setOnClickListener {
-            signOut()
-        }
-
-        view.btn_test_repo.setOnClickListener {
-            signInViewModel.printToConsole()
-        }
-
-        view.txt_current_user.text = signInViewModel.currentUser.value?.displayName.toString()
-        view.txt_current_user_token.text =
-            signInViewModel.currentUsersFirebaseToken.value?.token.toString()
+//        view.btn_log_out.setOnClickListener {
+//            signOut()
+//        }
+//
+//        view.btn_test_repo.setOnClickListener {
+//            signInViewModel.printToConsole()
+//        }
+//
+//        view.txt_current_user.text = signInViewModel.currentUser.value?.displayName.toString()
+//        view.txt_current_user_token.text =
+//            signInViewModel.currentUsersFirebaseToken.value?.token.toString()
 
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupObservers()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun setupObservers() {
+        electricityConsumptionPanel.apply {
+//            onInfoButtonClick
+        }
     }
 
     private fun signOut() {
---
+++ app/src/main/java/com/example/sdgbachelorproject/view/widget/ConsumptionView.kt
@@ -0,0 +1,49 @@
+package com.example.sdgbachelorproject.view.widget
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.sdgbachelorproject.R
+import io.reactivex.subjects.PublishSubject
+import kotlinx.android.synthetic.main.view_consumption.view.*
+
+class ConsumptionView: ConstraintLayout {
+
+    val onInfoButtonClick = PublishSubject.create<Unit>()
+    val onAddConsumptionButtonClick = PublishSubject.create<Unit>()
+
+    constructor(context: Context) : super(context) {
+        LayoutInflater.from(context).inflate(R.layout.view_consumption, this)
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : this(context, attrs, 0)
+
+    constructor(context: Context, attrs: AttributeSet, defstyle: Int) : super(context, attrs, defstyle) {
+        LayoutInflater.from(context).inflate(R.layout.view_consumption, this)
+        init(context, attrs)
+    }
+
+    private fun init(context: Context, attrs: AttributeSet) {
+        val arr = context.obtainStyledAttributes(attrs, R.styleable.ConsumptionView)
+        val consumptionTitleText = arr.getText(R.styleable.ConsumptionView_consumptionTitleText)
+        val consumptionValueText = arr.getText(R.styleable.ConsumptionView_consumptionValueText)
+        val consumptionIcon = arr.getDrawable(R.styleable.ConsumptionView_consumptionIcon)
+
+        consumption_name.text = consumptionTitleText
+        consumption_value.text = consumptionValueText
+        ic_consumption_icon.background = consumptionIcon
+        setupListeners()
+    }
+
+    private fun setupListeners() {
+        consumption_value_info.setOnClickListener {
+            onInfoButtonClick.onNext(Unit)
+        }
+
+        btn_add_consumption.setOnClickListener {
+            onAddConsumptionButtonClick.onNext(Unit)
+        }
+
+    }
+}
