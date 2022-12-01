f1d0e7ced56b9fe25256fe006a98a925379d8c48
WojtekMielczarek-Makimo <wojciech.mielczarek@makimo.pl>
2022-12-01 14:16:02 +0100

    Add barcode scanner

--- app/src/main/java/com/example/sdgbachelorproject/view/NutritionFragment.kt
+++ app/src/main/java/com/example/sdgbachelorproject/view/NutritionFragment.kt
@@ -1,60 +1,36 @@
 package com.example.sdgbachelorproject.view
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.sdgbachelorproject.R
+import androidx.fragment.app.Fragment
+import com.example.sdgbachelorproject.databinding.FragmentNutritionBinding
+import kotlinx.android.synthetic.main.fragment_nutrition.view.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [NutritionFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class NutritionFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private var _binding: FragmentNutritionBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_nutrition, container, false)
+        _binding = FragmentNutritionBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        view.btn_camera.setOnClickListener {
+            val intent = Intent(this.requireContext(), CameraActivity::class.java)
+            startActivity(intent)
+        }
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FifthFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            NutritionFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
